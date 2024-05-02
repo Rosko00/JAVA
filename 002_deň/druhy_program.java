07a0cd7fadb770e49f38e12b8c2eff8157770952
Marián Gálik <marian.galik@gmail.com>
2024-05-02 16:08:53 +0200

    2_deň_vzorové_kódy

---
+++ 002_deň/druhy_program.java
@@ -0,0 +1,17 @@
+/*
+ * Toto je ďalší klasický príklad.
+ * Tento súbor uložíme pod názvom druhy_program.java.
+ */
+class Example_2 {
+    public static void main(String[] args) {
+        int num; //Tento riadok deklaruje premennú z názvom num
+
+        num = 100; //Tento riadok premennej num priraďuje hodnotu 100
+        System.out.println ("Hodnota num je: " + num);
+
+        num = num * 2;
+
+        System.out.print ("Hodnota num * 2 je: ");
+        System.out.println (num);
+    }
+}
