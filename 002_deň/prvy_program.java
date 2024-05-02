90c645154e7818dbebfcc9841535ca3471857622
Marián Gálik <marian.galik@gmail.com>
2024-05-02 15:59:54 +0200

    hmm

---
+++ 002_deň/prvy_program.java
@@ -0,0 +1,10 @@
+/*Toto je jednoduchý java program
+ * Tento subor nazveme prvy_progrma.java
+ */
+class Example {
+//Náš program zacina volaním main ()
+    public static void main(String args[]) {
+        System.out.println("Toto je jednoduchý Java program.");
+        
+}
+}
