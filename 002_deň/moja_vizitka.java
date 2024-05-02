61367d9ddef1d88e879c010513350a942ac35fcc
Marián Gálik <marian.galik@gmail.com>
2024-05-02 18:57:18 +0200

    moja_vizitka

---
+++ 002_deň/moja_vizitka.java
@@ -0,0 +1,23 @@
+/*Toto sú moje základné informácie
+ * 
+*/
+class moja_vizitka {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String meno;
+        meno = "Marián Gálik";
+        String email;
+        email = "marian.galik@gmail.com";
+        String adresa;
+        adresa = "Dlhé lúky 6004/9, 020 01 Púchov";
+        String mobil;
+        mobil = "+421 (0)911 713 217";
+
+        System.out.println(meno);
+        System.out.println(email);
+        System.out.println(adresa);
+        System.out.println(mobil);
+    }
+}
