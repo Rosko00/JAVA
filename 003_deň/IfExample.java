43c5f801207749741635107365e9193f4208f6a6
Marián Gálik <marian.galik@gmail.com>
2024-05-07 16:34:40 +0200

    mmm

---
+++ 003_deň/IfExample.java
@@ -0,0 +1,9 @@
+public class IfExample {
+  public static void main(String[] args) {
+      int number = 20;
+
+      if (number > 10) {
+          System.out.println("Číslo je väčšie ako 10.");
+      }
+  }
+}
--- 003_deň/podmienka_if.java
+++
@@ -1,8 +0,0 @@
-/* Ukazkove programy na podmienku if */
-public class podmienka_if { 
-    public static void main(String[] args) {
-        if (20 > 18) {
-            System.out.println("20 je viac ako 18"); 
-    }  
-  }
-}
