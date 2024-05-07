3d400f4f58f25217520b03f8c3d7a4e75c0c45f6
Marián Gálik <marian.galik@gmail.com>
2024-05-07 15:56:52 +0200

    update

--- 002_deň/prvy_program.java
+++ 002_deň/prvy_program.java
@@ -2,9 +2,9 @@
  * Tento subor nazveme prvy_progrma.java
  */
 class Example {
-//Náš program zacina volaním main ()
-    public static void main(String args[]) {
-        System.out.println("Toto je jednoduchý Java program.");
-        
-}
-}
+    //Náš program zacina volaním main ()
+        public static void main(String args[]) {
+            System.out.println("Toto je jednoduchý Java program.");
+            
+    }
+    }
