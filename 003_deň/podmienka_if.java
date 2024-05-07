986f55591112c038ef1729062d0f4266f0d655fb
Marián Gálik <marian.galik@gmail.com>
2024-05-07 16:31:08 +0200

    hmm

---
+++ 003_deň/podmienka_if.java
@@ -0,0 +1,8 @@
+/* Ukazkove programy na podmienku if */
+public class podmienka_if { 
+    public static void main(String[] args) {
+        if (20 > 18) {
+            System.out.println("20 je viac ako 18"); 
+    }  
+  }
+}
