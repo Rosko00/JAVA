95bdf5dcf91588a8e6cc5ea5f364887de73c8f53
Marián Gálik <marian.galik@gmail.com>
2024-05-07 18:20:08 +0200

    for_cyklus

---
+++ 003_deň/forExample.java
@@ -0,0 +1,9 @@
+public class forExample {
+    public static void main(String[] args) {
+        // Vytvorenie for cyklu, ktorý vykonáva 5 iterácií
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iterácia číslo: " + i);
+        }
+    }
+}
+
