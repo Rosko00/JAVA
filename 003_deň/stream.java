e6004d76698bdf36569240939825ab35686c1221
Marián Gálik <marian.galik@gmail.com>
2024-05-11 18:39:45 +0200

    Create stream.java

---
+++ 003_deň/stream.java
@@ -0,0 +1,10 @@
+/* if (podmienka) príkaz; */
+public class stream {
+    public static void main(String[] args) {
+        int number = 20;
+
+        if (number >10) {
+            System.out.println ("Číslo je väčšie ako 10.");
+        }
+    }
+}
