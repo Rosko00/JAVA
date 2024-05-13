9f03561b52075bc46988c8005a20118d7c4bf6cd
Marián Gálik <marian.galik@gmail.com>
2024-05-13 11:19:10 +0200

    hmm

---
+++ 004_deň/slova.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class slova {
+    public static void main(String[] args) {
+        String[] keywords = {
+            "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float",
+            "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native",
+            "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp",
+            "super", "switch", "synchronized", "this", "throw", "throws", "transient", "try", "void",
+            "volatile", "while"
+        };
+        
+        System.out.println("Klíčová slova v jazyce Java:");
+        for (String keyword : keywords) {
+            System.out.println(keyword);
+        }
+    }
+}
