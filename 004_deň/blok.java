78ad16c918b00c9ea26880cec67bdb70aaa13513
Marián Gálik <marian.galik@gmail.com>
2024-05-13 11:01:22 +0200

    hmm

---
+++ 004_deň/blok.java
@@ -0,0 +1,20 @@
+public class blok {
+    public static void main(String[] args) {
+        // Blok kódu 1: Deklarace a inicializace proměnných
+        int x = 5;
+        int y = 3;
+        
+        // Blok kódu 2: Podmíněný výraz pro porovnání čísel
+        if (x > y) {
+            System.out.println("x je větší než y");
+        } else {
+            System.out.println("x není větší než y");
+        }
+        
+        // Blok kódu 3: Cyklus pro vypsání čísel od 1 do 5
+        System.out.println("Čísla od 1 do 5:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
