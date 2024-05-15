51e25854f822ab70c9e96aea2dd4e25b2eef6ac5
Marián Gálik <marian.galik@gmail.com>
2024-05-15 10:12:37 +0200

    hmm

---
+++ 005_deň/kruh.java
@@ -0,0 +1,13 @@
+public class kruh {
+    public static void main(String[] args) {
+       double pi, r, a;
+ 
+       r = 10.8; // poloměr kruhu
+       pi = 3.14; // přibližná hodnota pi
+ 
+       a = pi * r * r; // výpočet plochy
+ 
+       System.out.println("Plocha kruhu je: " + a);
+    } 
+ }
+ 
---
+++ 005_deň/test.java
@@ -0,0 +1,16 @@
+public class test {
+    public class BooleanExample {
+    public static void main(String[] args) {
+        // Deklarace booleanových proměnných
+        boolean pravda = true;
+        boolean nepravda = false;
+
+        // Výpis hodnot booleanových proměnných
+        System.out.println("Hodnota pravda: " + pravda);
+        System.out.println("Hodnota nepravda: " + nepravda);
+
+        
+    }
+}
+
+}
