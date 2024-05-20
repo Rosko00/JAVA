122c28e113515461aa021b6ab97d022343211482
Marián Gálik <marian.galik@gmail.com>
2024-05-20 22:17:52 +0200

    m

---
+++ 006_deň/premenne.java
@@ -0,0 +1,25 @@
+public class premenne {
+    public static void main(String[] args) {
+        // Deklarácia a inicializácia celého čísla
+        int vek = 25;
+        
+        // Deklarácia a inicializácia desatinného čísla
+        double hmotnost = 70.5;
+        
+        // Deklarácia a inicializácia reťazca
+        String meno = "Ján";
+
+        // Deklarácia a inicializácia booleanovej hodnoty
+        boolean jeStudent = true;
+
+        // Vypísanie hodnôt premenných
+        System.out.println("Meno: " + meno);
+        System.out.println("Vek: " + vek);
+        System.out.println("Hmotnosť: " + hmotnost);
+        System.out.println("Je študent: " + jeStudent);
+
+        // Zmena hodnoty premennej
+        vek = 26;
+        System.out.println("Aktualizovaný vek: " + vek);
+    }
+}
