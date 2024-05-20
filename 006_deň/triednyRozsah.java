45c411b707bf99859c993410fde0d786c58ee495
Marián Gálik <marian.galik@gmail.com>
2024-05-20 22:35:06 +0200

    hmm

---
+++ 006_deň/triednyRozsah.java
@@ -0,0 +1,12 @@
+public class triednyRozsah {
+    private int triednaPremenna = 10; // Triedna premenná
+
+    public void ukazTriednuPremennu() {
+        System.out.println("Hodnota triednaPremenna: " + triednaPremenna);
+    }
+
+    public static void main(String[] args) {
+        TriednePremenne obj = new TriednePremenne();
+        obj.ukazTriednuPremennu();
+    }
+}
