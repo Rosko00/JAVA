20779ae9d1d5dc62ba8fd1f351d43fac04bea811
Marián Gálik <marian.galik@gmail.com>
2024-05-24 09:58:46 +0200

    hotovo

--- 007_deň/viac_dimenzi_pole_1.java
+++ 007_deň/viac_dimenzi_pole_1.java
@@ -1,5 +1,28 @@
-/* Rucna alokacia poli druhej dimenzie o roznej dlzke */
 public class viac_dimenzi_pole_1 {
+    public static void main(String[] args) {
+        // Deklarácia 2D poľa s rôznymi dĺžkami vnútorných polí
+        int al_pola [][] = new int [4][];
+        al_pola [0] = new int [1]; // Pole s dĺžkou 1
+        al_pola [1] = new int [2]; // Pole s dĺžkou 2
+        al_pola [2] = new int [3]; // Pole s dĺžkou 3
+        al_pola [3] = new int [4]; // Pole s dĺžkou 4
 
-    
+        int i, j, k = 0;
+
+        // Naplnenie poľa hodnotami
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < i + 1; j++) {
+                al_pola[i][j] = k;
+                k++;
+            }
+        }
+        
+        // Vypísanie poľa
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < i + 1; j++) {
+                System.out.print(al_pola[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- 007_deň/viac_dimenzi_pole_3.java
+++ 007_deň/viac_dimenzi_pole_3.java
@@ -1,5 +1,22 @@
 /* Ukazka prace s trojdimenzionalnym polom */
 public class viac_dimenzi_pole_3 {
+    public static void main(String[] args) {
+        int troj_pole [][][] = new int [3][4][5];
+        int i, j, k;
 
-    
+        for (i=0; i<3; i++)
+            for (j=0; j<4; j++)
+                for (k=0; k<5; k++)
+                    troj_pole [i][j][k] = i +j + k;
+                        
+                        for (i=0; i<3; i++){
+                            for (j=0; j<4; j++){
+                                for (k=0; k<5; k++){
+                                    System.out.print (troj_pole [i][j][k] + " ");
+                                System.out.println();
+                                }
+                            System.out.println();
+                            }
+                        }
+    }    
 }
