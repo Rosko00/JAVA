4e0040dea01d4860660e58731610c2b5828f1f60
Marián Gálik <marian.galik@gmail.com>
2024-05-24 11:36:41 +0200

    n

--- 008_deň/blok_kodu_2.java
+++ 008_deň/blok_kodu_2.java
@@ -1,3 +1,14 @@
 public class blok_kodu_2 {
-    
+    public static void main(String[] args) {
+        int number = 10;
+        
+        if (number > 0) {
+            System.out.println("Číslo je kladné.");
+        } else if (number < 0) {
+            System.out.println("Číslo je záporné.");
+        } else {
+            System.out.println("Číslo je nula.");
+        }
+    }
+}
 }
