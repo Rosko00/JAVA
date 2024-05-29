6672ba5386243e64f047b41a15246ac12eb9631c
Marián Gálik <marian.galik@gmail.com>
2024-05-29 09:18:02 +0200

    3

---
+++ 009_deň/aritmeticke_operatory.java
@@ -0,0 +1,16 @@
+/* Aritmetické operátory */
+public class aritmeticke_operatory {
+    
+}
+// ( +  ) = Súčet
+// ( -  ) = Rozdiel
+// ( *  ) = Súčin
+// ( /  ) = Podiel
+// ( %  ) = Modulo ( Zbytok po delení celých čísel)
+// ( ++ ) = Inkrement
+// ( += ) = Priradenie súčtu
+// ( -= ) = Priradenie rozdielu
+// ( *= ) = Priradenie súčinu
+// ( /= ) = Priradenie podielu
+// ( %= ) = Priradenie modula
+// ( -- ) = Dekrement
