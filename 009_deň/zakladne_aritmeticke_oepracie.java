d227b618f66e7c210037a62905662a6bbb13ac6a
Marián Gálik <marian.galik@gmail.com>
2024-06-01 12:24:07 +0200

    hmm

---
+++ 009_deň/zakladne_aritmeticke_oepracie.java
@@ -0,0 +1,24 @@
+public class zakladne_aritmeticke_oepracie {
+    public static void main(String[] args) {
+        
+        // Celé čísla
+        System.out.println ("Aritmetika s celými číslami:");
+
+        int a = 1 + 1 ;
+        int b = a * 3 ;
+        int c = b / 4 ;
+        int d = c - a ;
+        int e = -d;
+
+        System.out.println (" a = " + a );
+        System.out.println (" b = " + b );
+        System.out.println (" c = " + c );
+        System.out.println (" d = " + d );
+        System.out.println (" e = " + e );
+
+        // Desatinné čásla
+        System.out.println("Aritmetika z desatinnými číslami:");
+
+        
+    }
+}
