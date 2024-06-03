caf7ee605a2cf4c71eb3120b8e307eb9cd6e6dab
Marián Gálik <marian.galik@gmail.com>
2024-06-03 11:33:12 +0200

    skuska

--- 009_deň/zlozene_aritmeticke_operacie_priradenie_1.java
+++ 009_deň/zlozene_aritmeticke_operacie_priradenie_1.java
@@ -1,15 +1,11 @@
-public class zlozene_aritmeticke_operacie_priradenie {
+public class zlozene_aritmeticke_operacie_priradenie_1 {
    public static void main(String[] args) {
+       
         //Zložené aritmetické operácie
         int v = 4;
         int q = v + 4;
 
         System.out.println("hodnota a je: " + v);
         System.out.println("hodnota a je: " + q);
-
-        //Kračšia alternatíva
-        int z = v +=5;
-        System.out.println("hodnota a je: " + z);
-    
    } 
 }
---
+++ 009_deň/zlozenie_aritmeticke_operacie_priradenie_2.java
@@ -0,0 +1,10 @@
+public class zlozenie_aritmeticke_operacie_priradenie_2 {
+    public static void main(String[] args) {
+
+        //Zložené aritmetické operácie
+        int a = 0;
+        int a += 4;
+        System.out.println("Hodnota premennej a je: " + a);
+    }
+    
+}
