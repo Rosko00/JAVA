57b1fb8c09025c357be40792e996685132f653fa
Marián Gálik <marian.galik@gmail.com>
2024-06-13 08:51:53 +0200

    hmm

---
+++ 011_deň/relacne_operatory.java
@@ -0,0 +1,9 @@
+public class relacne_operatory {
+    public static void main(String[] args) {
+        // Rovnosť
+        int a = 5;
+        int b = 10;
+        boolean result = (a == b); // result bude false
+        System.out.println(result);
+    }
+}
