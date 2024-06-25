1bbab943b2df17f0552a89028fbf47b0743ae615
Marián Gálik <marian.galik@gmail.com>
2024-06-25 14:00:16 +0200

    skuska

---
+++ 013_deň/stream.java
@@ -0,0 +1,12 @@
+public class stream {
+    public static void main(String[] args) {
+        // výraz_1 ? výraz_2 : výraz_3
+
+        int i, k;
+        i = 10;
+        k = i < 0 ? -i : i; // vypocet absolutnej hodnoty i
+        System.out.println( i + "je" + k);
+        
+    }
+    
+}
