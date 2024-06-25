27be0a7c0fc6c70ba467940e6cbff8cff61723ea
Marián Gálik <marian.galik@gmail.com>
2024-06-25 15:32:41 +0200

    update

--- 013_deň/stream.java
+++ 013_deň/stream.java
@@ -3,10 +3,23 @@ public class stream {
         // výraz_1 ? výraz_2 : výraz_3
 
         int i, k;
-        i = 10;
-        k = i < 0 ? -i : i; // vypocet absolutnej hodnoty i
-        System.out.println( i + "je" + k);
         
+        i = 10;
+        k = i < 0 ? -i : i; // Výpočet absolutnej hodnoty i
+        System.out.println( i + " je " + k);
+
+        i = -10;
+        k = i < 0 ? -i : i;
+        System.out.println( i + " je " + k);
+
+        // premenna = výraz;
+
+        int a, b, c;
+        a = b = c = 100;
+        System.out.println(a + b + c );
+
+        int ab = (b + 3);
+        System.out.println(ab);
     }
     
 }
