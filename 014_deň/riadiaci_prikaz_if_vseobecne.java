52ccaee3c38c62111fe82e632ec9537d5c00798a
Marián Gálik <marian.galik@gmail.com>
2024-06-28 12:26:20 +0200

    hmm

---
+++ 014_deň/riadiaci_prikaz_if_vseobecne.java
@@ -0,0 +1,12 @@
+public class riadiaci_prikaz_if_vseobecne {
+   public static void main(String[] args) {
+
+        if (Podmienka) { // blok kódu, ktorý sa vykoná, ak je podmienka pravdivá
+        }
+        else {
+            // blok kódu, ktorý sa vykoná, ak je podmienka nepravdivá
+        }
+        
+    
+   } 
+}
