7e5e098bd1f7bb37a493231509f18f166eb97a18
Marián Gálik <marian.galik@gmail.com>
2024-06-28 12:34:04 +0200

    uoravený kod

--- 014_deň/riadiaci_prikaz_if_vseobecne.java
+++ 014_deň/riadiaci_prikaz_if_vseobecne.java
@@ -1,11 +1,22 @@
 public class riadiaci_prikaz_if_vseobecne {
    public static void main(String[] args) {
 
-        if (Podmienka) { // blok kódu, ktorý sa vykoná, ak je podmienka pravdivá
-        }
-        else {
-            // blok kódu, ktorý sa vykoná, ak je podmienka nepravdivá
-        }
+    int vek = 18;
+
+    if (vek >= 18) {
+        System.out.println("Máš právo hlasovať.");
+    }
+
+    int vek_2 = 16;
+
+    if (vek_2 >= 18) {
+    System.out.println("Máš právo hlasovať.");
+    } 
+    else {
+    System.out.println("Nemáš právo hlasovať.");
+    }
+
+     
         
     
    } 
