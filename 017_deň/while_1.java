f606f83e77a520319f2a2e8537e87cbfe2da8505
Marián Gálik <marian.galik@gmail.com>
2024-07-04 16:41:56 +0200

    hmm

--- 017_deň/while_1.java
+++ 017_deň/while_1.java
@@ -1,9 +1,7 @@
 public class while_1 {
     public static void main(String[] args) {
         int a = 10, b = 20;
-        while (a>b)
+        while (a > b)
             System.out.println("Tento riadok sa nikdy nezobrazí.");
     }
-
-    
 }
