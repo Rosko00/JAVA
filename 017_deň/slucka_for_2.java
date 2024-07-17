cfa999eb776586b32f1ad1695ade7ecb07610207
Marián Gálik <marian.galik@gmail.com>
2024-07-17 09:38:28 +0200

    hmm

---
+++ 017_deň/slucka_for_2.java
@@ -0,0 +1,8 @@
+/* Deklarovanie riadiacej premennej vo vnútri slučky príkazu for */
+public class slucka_for_2 {
+    public static void main(String[] args) {
+        for (int n=10; n>0; n--)
+            System.out.printlj("číslo " + n);
+        
+    }
+}
