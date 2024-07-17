70334b784ea050fc192f2b9cca391526e8fb162d
Marián Gálik <marian.galik@gmail.com>
2024-07-17 09:29:43 +0200

    hmm

---
+++ 017_deň/for_zaklad.java
@@ -0,0 +1,15 @@
+public class for_zaklad {
+    public static void main(String[] args) {
+        for (initialization; termination; increment) {
+            // Body of the loop (telo slučky)
+        }
+    }
+}
+/* Initialization (inicializácia): Tento výraz sa vykoná iba raz, 
+   na začiatku cyklu. Zvyčajne sa tu inicializuje premenná alebo viaceré premenné. */
+
+/* Termination (ukončenie): Tento výraz sa vyhodnocuje pred každou iteráciou cyklu. 
+   Pokiaľ je tento výraz true, cyklus pokračuje, inak sa cyklus ukončí. */
+
+/* Increment (inkrementácia): Tento výraz sa vykoná po každej iterácii cyklu. 
+Často sa používa na zvýšenie hodnoty premenných inicializovaných v kroku inicializácie. */
