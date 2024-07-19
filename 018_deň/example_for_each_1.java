1765de22c19e4ffd25ec05f3ca5193cb0ce43439
Marián Gálik <marian.galik@gmail.com>
2024-07-19 14:58:45 +0200

    ubdate

--- 018_deň/example_for_each_1.java
+++ 018_deň/example_for_each_1.java
@@ -1,9 +1,9 @@
 public class example_for_each_1 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int sum = 0;
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
         sum += nums[i];
         }
 
--- 018_deň/stream.java
+++ 018_deň/stream.java
@@ -1,4 +1,19 @@
 public class stream {
     public static void main(String[] args) {
+        int [] nums = {1,2,3,4,5,6,7,8,9,10};
+
+        for (int x : nums) {
+            System.out.print ( x + "");
+            x = x * 10; // Toto nebude mať žiadny vplyv na pole
+        }
+        
+        System.out.printl();
+
+        for (int x : nums) {
+            System.out.print ( x + "");
+        }
+
+        System.out.println();
+
     }
 }
