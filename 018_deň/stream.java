d8fae5eba54ffcbb27536e1449cb3e839e4ed750
Marián Gálik <marian.galik@gmail.com>
2024-07-19 14:32:10 +0200

    delete

--- 018_deň/stream.java
+++ 018_deň/stream.java
@@ -1,13 +1,4 @@
 public class stream {
     public static void main(String[] args) {
-        int [] nums = {1,2,3,4,5,6,7,8,9};
-        int sum = 0;
-
-        for (int i = 0; i < 9; i ++) {
-        sum += nums[i];
-        }
-
-        System.out.println("Sum: " + sum);
-        
     }
 }
