bce4b8f3c2480809bf6b2ad720f2a56e9023cb3a
Marián Gálik <marian.galik@gmail.com>
2024-07-23 21:37:45 +0200

    hmm

---
+++ 019_deň/continue_1.java
@@ -0,0 +1,10 @@
+public class continue_1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i ++) {
+            if ( i == 10) break; // ukončenie slučky ak je i == 10
+            System.out.println(" i: " + i );
+        }
+        System.out.println(" Slučka ukončená. ");
+    }
+    
+}
