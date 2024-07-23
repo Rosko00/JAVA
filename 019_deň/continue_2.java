e524b946aef25183efafe71a1f241531201e2d66
Marián Gálik <marian.galik@gmail.com>
2024-07-23 21:55:25 +0200

    update

---
+++ 019_deň/continue_2.java
@@ -0,0 +1,13 @@
+public class continue_2 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        while ( i < 100) {
+            if ( i == 10) break; // ukončenie slučky ak sa i rovná 10
+            System.out.println(" i :" + i);
+                i ++;
+
+        }
+        System.out.println("Slučka ukončená");
+    }
+}
---
+++ 019_deň/continue_3.java
@@ -0,0 +1,13 @@
+public class continue_3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i ++) {
+            System.out.println(" Prechod " + i + " :");
+            for (int j = 0; j< 100; j++){
+                if ( j == 10) break; // Ukončenie slučky
+                System.out.print( j + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Všetky slučky ukončené.");
+    }
+}
