743bccf519b34d35c8378696d3669c4909ae1a9b
Marián Gálik <marian.galik@gmail.com>
2024-07-28 11:26:58 +0200

    hmm

---
+++ krabica_2.java
@@ -0,0 +1,7 @@
+class krabica_2 {
+    double width;
+    double height;
+    double depth;
+
+    krabica_2 mojakrabica = new krabica_2(); // Vytvorí objekt triedy krabica_2 s názvom mojakrabica
+}
