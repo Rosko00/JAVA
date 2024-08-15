663b5c0763da2d9672150ce97099520ccb79e06c
Marián Gálik <marian.galik@gmail.com>
2024-08-15 12:06:35 +0200

    hmm

---
+++ 022_deň/metoda_01.java
@@ -0,0 +1,12 @@
+public class metoda_01 {
+    double sirka;
+    double vyska;
+    double hlbka;
+
+    // vypočet a zobrazenie objemu krabica
+    void objem () {
+        System.out.println("Objem krabice je: ");
+        System.out.println(sirka + vyska + hlbka);
+    }
+}
+
