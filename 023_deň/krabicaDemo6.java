daab78b158f7efcd4e4b16ee16221d6fff673398
Marián Gálik <marian.galik@gmail.com>
2024-08-20 19:45:03 +0200

    hmm

--- 023_deň/krabicaDemo6.java
+++ 023_deň/krabicaDemo6.java
@@ -1,5 +1,4 @@
 // Trieda krabica a použitie konštruktora
-
 class krabica {
     double sirka;
     double vyska;
@@ -21,5 +20,20 @@ class krabica {
 }    
 
 class krabicaDemo6 {
-    
+    public static void main(String[] args) {
+        
+        // Deklarácia alokácia a inicializácia objektu triedy krabuica
+        krabica mojakrabica1 = new krabica();
+        krabica mojakrabica2 = new krabica();
+
+        double obj;
+
+        // Načítanie objemu prvý krabica
+        obj = mojakrabica1.objem();
+        System.out.println("Objem krabica 1 je " + obj);
+
+        // Načítanie objemu druhá krabica
+        obj = mojakrabica2.objem();
+        System.out.println("Objem krabice 2 je " + obj);
+    }   
 }
